d8ad97ba75aa67f35b9c0658d546e4f7eb0df4ed
User <w2030@e-mirim.hs.kr>
2022-03-25 14:46:23 +0900

    정적서버페이지

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http = require('http');
+const fs = require("fs")
 
 const app = http.createServer(function (req,res) {
+
+    let url = req.url;
+
+    if(url === '/')
+        url = '/index.html';
+    if(url === '/favoricon.ico')
+        return res.writeHead(404);
+
     res.writeHead(200);
-    console.log(__dirname);
-    res.end("Hello World");
+    res.end(fs.readFileSync(__dirname + url));
+    // res.writeHead(200);
+    // res.end(fs.readFileSync());
+    //
+    // console.log(__dirname);
+    // res.end("Hello World");
 });
 
-app.listen(3000);
+app.listen(3333);
