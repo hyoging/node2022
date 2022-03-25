6d6a0a6615215beb519f39736e209b231d2f70b8
User <w2030@e-mirim.hs.kr>
2022-03-25 15:06:41 +0900

    wdb2 간단한 동적 서버페이지

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const http = require('http');
 const fs = require("fs")
+const url = require('url');
 
 const app = http.createServer(function (req,res) {
 
     let url = req.url;
+    let queryData = url.parse(_url, true).query;
+    res.end(queryData.id);
 
-    if(url === '/')
-        url = '/index.html';
-    if(url === '/favoricon.ico')
-        return res.writeHead(404);
-
-    res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + url));
+    // if(url === '/')
+    //     url = '/index.html';
+    // if(url === '/favoricon.ico')
+    //     return res.writeHead(404);
+    //
+    // res.writeHead(200);
+    // res.end(fs.readFileSync(__dirname + url));
     // res.writeHead(200);
     // res.end(fs.readFileSync());
     //
