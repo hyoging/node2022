6479a19fc2f36d0c0864f938acc60f8bed1e028e
hyoging <e-mirim.hs.kr>
2022-06-13 15:11:18 +0900

    route, create, read 구현

---
+++ controllers/employeeController.js
@@ -0,0 +1,56 @@
+const Employee = require('../models/employeeModel')
+
+// Employee들의 모든 list를 보여줌
+const showAll = function (req, res, next){
+    Employee.find()
+        .then(function(response){
+            res.json({
+                response
+            })
+        })
+        .catch(function (error){
+            res.json({
+                message: 'showAll 에러발생'
+            })
+        })
+}
+const show = function (req, res, next){
+    const employeeID = req.body_id
+    Employee.findById(employeeID)
+        .then(function(response){
+            res.json({
+                response
+            })
+        })
+        .catch(function (error){
+            res.json({
+                message: 'show 에러발생'
+            })
+        })
+}
+
+
+// Employee 데이터 INSERT
+const insert =function (req, res, next){
+    const data = new Employee({
+        name : req.body.name,
+        age : req.body.age,
+        phone : req.body.phone
+    })
+
+    data.save()
+        .then(function (response){
+            res.json({
+                message: '데이터 삽입 성공'
+            })
+        })
+        .catch(function (error){
+            res.json({
+                message: 'insert 에러 발생'
+            })
+        })
+}
+
+module.exports - {
+    showAll, show, insert
+}
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const app = express()
+mongoose.connect(process.env.DB_CONNECTION
+, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection;
+db.on('error', function (err){
+    console.log(err)
+})
+
+db.once('open', function (){
+    console.log('디비 연결')
+})
+
+const EmployeeRoute = require('./routes/employeeRoutes')
+
 
 app.use(morgan('dev'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/api/employee', EmployeeRoute)
+
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT)
